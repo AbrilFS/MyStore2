f52113ffa2a5eb867889331c8f26989ee3556316
Abril Flores Silva <abril.qfs@gmail.comgit remote add origin git@github.com:AbrilFS/preworkMAC.gitgithhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhellohow areyoudoingtoday!>
2023-02-27 19:40:46 -0600

    Haciendo peticiones a la API

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { StoreService } from '../../services/store.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 
 @Component({
@@ -12,37 +13,20 @@ export class ProductsComponent {
 
   myShoppingCart: Product[] = [] //Cantidad de productos en el carrito.
   total = 0//variable del total de productos en el carrito.
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-  ];
+  products: Product[] = [];
 
   constructor(
-    private storeService : StoreService
+    private storeService : StoreService,
+    private productService : ProductsService
   ) {
     this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
+  ngOnInit (): void{
+    this.productService.getAllProducts()
+    .subscribe(data =>{
+      this.products = data
+    });
   }
 
   //método para agregar productos al carrito
